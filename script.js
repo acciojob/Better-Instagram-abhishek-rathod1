18948a5548adfe8dc3df6ee262255a03f42d6511
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-24 15:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+
+let dragDiv = null;
+
+function dragstartHandler(e){
+	e.dataTransfer.setData("text",e.target.id);
+		
+}
+
+function dragoverHandler(e) {
+	e.preventDefault();
+}
+
+function dropHandler(e) {
+	e.preventDefault();
+	const data = e.dataTransfer.getData("text");
+	e.target.appendChild(document.getElementById(data));
+}
+
